8fad48f26da781c7f9cb450f9fd9a987cb13816d
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2023-01-14 01:24:47 +0300

    lecture 1 part-1

--- project-one/app.ts
+++ project-one/app.ts
@@ -1,1 +1,11 @@
-console.log('Your code goes here...');
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
+
+printResult(add(5, 12));
+
+//let someValue: undefined;
---
+++ project-one/basics.ts
@@ -0,0 +1,18 @@
+function add(num1: number, num2: number, showResult: boolean, phrase: string) {
+  //   if (typeof num1 !== "number" || typeof num2 !== "number") {
+  //     throw new Error("Incorrect input!");
+  //   }
+  const result = num1 + num2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
+
+const number1 = 5; // 5.0
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+const result = add(number1, number2, printResult, resultPhrase);
