ad70d95c8725aae061d2ae0b26bb761e65e29d9b
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2023-01-18 23:03:30 +0300

    lecture-3 finished

--- project-two/src/app.ts
+++ project-two/src/app.ts
@@ -1,7 +1,13 @@
 const button = document.querySelector("button");
 // const button = document.querySelector('button')!;
 
+function add(n1: number, n2: number){
+  if(n1+n2 >0)
+  return n1+ n2;
+}
+
 function clickHandler(message: string) {
+  //let userName = 'Can';
   console.log("Clicked!" + message);
 }
 
