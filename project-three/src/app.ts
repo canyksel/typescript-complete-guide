572724be70bba62ff5f0457147726ee5c188cddf
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2023-01-26 23:48:11 +0300

    lecture-4 part-3

--- project-three/src/app.ts
+++ project-three/src/app.ts
@@ -17,24 +17,53 @@ age = 30;
 // console.log(isOld);
 // console.log(result);
 
-const add = (a: number, b: number = 1) => a + b;
+// const add = (a: number, b: number = 1) => a + b;
 
 //const add = (a: number = 1, b: number) => a + b;  --> bu kullanım yanlıştır. Default argument sonda kullanılabilir.
 
 //const add = (a: number, b: number = 1) => a + b; --> default argument kullanımı.
 
-console.log(add(2, 5));
+// console.log(add(2, 5));
 
 // const printOutput = (output: string | number) =>{
 //     console.log(output);
 // }
 
-const printOutput: (a: number | string) => void = output =>console.log(output);
+// const printOutput: (a: number | string) => void = (output) =>
+//   console.log(output);
 
-printOutput(add(5));
+// const button = document.querySelector("button");
 
-const button = document.querySelector('button');
+// if (button) {
+//   button.addEventListener("click", (event) => console.log(event));
+// }
 
-if(button){
-    button.addEventListener('click', event => console.log(event));
-}
+// printOutput(add(5));
+
+const hobbies = ["Sports", "Cooking"];
+const activeHobbies = ["Hiking"];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  name: "Can",
+  age: 29,
+};
+
+const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+// Length belli ise
+// const add = (...numbers: [number, number,number]) => {
+//     return numbers.reduce((curResult, curValue) => {
+//       return curResult + curValue;
+//     }, 0);
+//   };
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
