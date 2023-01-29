eaff70fce996aabea72eec83827bea1dde7c9480
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2023-01-29 23:45:38 +0300

    lecture-5 part-2

--- project-four/src/app.ts
+++ project-four/src/app.ts
@@ -1,1 +1,37 @@
-// Code goes here!
+class Department {
+  //   private readonly id: string;
+  //   private name: string;
+  private employees: string[] = [];
+
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const accounting = new Department("d1", "Accounting");
+
+accounting.addEmployee("Can");
+accounting.addEmployee("Test");
+
+// accounting.employees[2] = "Yüksel";
+
+accounting.describe();
+accounting.name = "NEW NAME";
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+
+// accountingCopy.describe();
