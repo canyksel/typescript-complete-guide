1fcafcc3ce70e68fb18566d188848a35acd37dba
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2023-01-31 23:53:17 +0300

    lecture-5 part-4

--- project-four/src/app.ts
+++ project-four/src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   //   private readonly id: string;
   //   private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -22,19 +22,63 @@ class Department {
   }
 }
 
-
 class ITDepartment extends Department {
-
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
 }
-const accounting = new ITDepartment("d1", "Accounting");
 
-accounting.addEmployee("Can");
-accounting.addEmployee("Test");
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
+
+  addEmployee(name: string): void {
+    if (name === "Can") return;
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+// const accounting = new ITDepartment("d1",['Can']);
+
+// accounting.addEmployee("Can");
+// accounting.addEmployee("Test");
+
+// accounting.describe();
+// accounting.name = "NEW NAME";
+// accounting.printEmployeeInformation();
 
 // accounting.employees[2] = "Yüksel";
 
-accounting.describe();
-accounting.name = "NEW NAME";
+const it = new ITDepartment("d1", ["Can"]);
+
+it.addEmployee("Can");
+it.addEmployee("Test");
+
+it.describe();
+it.name = "NEW NAME";
+it.printEmployeeInformation();
+
+console.log(it);
+
+const accounting = new AccountingDepartment("d2", []);
+
+accounting.addEmployee('Can');
+accounting.addEmployee('Test');
+
+
+accounting.addReport("Something went wrong...");
+accounting.printReports();
 accounting.printEmployeeInformation();
 // const accountingCopy = { name: "DUMMY", describe: accounting.describe };
 
