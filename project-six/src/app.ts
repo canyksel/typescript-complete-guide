99c3e80e84db23e12b330df1977bf0f4c3016d3d
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2023-03-14 23:46:14 +0300

    lecture-7 part-2

--- project-six/src/app.ts
+++ project-six/src/app.ts
@@ -1,1 +1,79 @@
-// Code goes here!
+// const names: Array<string> = []; //string[];
+// //names[0].split('');
+
+// const promise: Promise<any> = new Promise((resolve, reject) =>{
+//     setTimeout(() =>{
+//         resolve('This is done!');
+//     },2000);
+// })
+
+// promise.then(data =>{
+//     data.split(' ');
+// })
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Can", hobbies: ["Sports"] }, { age: 29 });
+console.log(mergedObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(["Sports", "Cooking"]));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+
+extractAndConvert({ name: "Can" }, "name");
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1); //-1
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+
+textStorage.addItem("Can");
+textStorage.addItem("Test");
+textStorage.removeItem("Test");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// const canObj = { name: "Can" };
+// objStorage.addItem(canObj);
+// objStorage.addItem({ name: "Test" });
+// objStorage.removeItem(canObj);
+// console.log(objStorage.getItems());
