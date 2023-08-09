5bb159059138e4140908cb2851f603b660c783ed
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2023-08-09 22:16:17 +0300

    lecture-10 finished

--- project-eight/src/components/project-list.ts
+++ project-eight/src/components/project-list.ts
@@ -1,12 +1,12 @@
-/// <reference path="base-component.ts" />
-/// <reference path="../decorators/autobind.ts" />
-/// <reference path="../state/project-state.ts" />
-/// <reference path="../models/project.ts" />
-/// <reference path="../models/drag-drop.ts" />
+import { autobind } from "../decorators/autobind.js";
+import Component from "./base-component.js";
+import { DragTarget } from "../models/drag-drop.js";
+import { Project, ProjectStatus } from "../models/project.js";
+import { projectState } from "../state/project-state.js";
+import { ProjectItem } from "./project-item.js";
 
-namespace App{
-    // ProjectList Class
-  export class ProjectList
+// ProjectList Class
+export class ProjectList
   extends Component<HTMLDivElement, HTMLElement>
   implements DragTarget
 {
@@ -77,5 +77,4 @@ namespace App{
       new ProjectItem(this.element.querySelector("ul")!.id, prjItem);
     }
   }
-}
 }
